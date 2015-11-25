d2f014e21dad0d54c3da7ef00c76ccfb855db4ef
omkar999 <omkar.p89@gmail.com>
2015-11-25 17:57:24 +0530

    SpringMvcHibernate Project

---
+++ src/main/java/com/nextgen/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.nextgen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@ResponseBody
+	@RequestMapping("/greet")
+	public String greetUser() {
+		return "Hello...Welcome to MyApp1";
+	}
+	
+}
